c9aa2b4ba837ebe4eda79f435ff7c09e163659c2
Ant Redstone <32925592+Ant-Redstone@users.noreply.github.com>
2020-06-04 15:47:22 -0300

    bruhh

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* Copyright (C) 1883 Thomas Edison - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the XYZ license, which unfortunately won't be
- * written for another century.
- *
- * You should have received a copy of the XYZ license with
- * this file. If not, please write to: , or visit :
- */
-
-
 const fs = require('fs')
 const xml2js = require('xml2js');
  
